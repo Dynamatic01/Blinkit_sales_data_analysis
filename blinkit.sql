044f7359d811f7147608d7cdf84b92962e33db79
Mohit <mehtamohit514@gmail.com>
2026-07-08 22:12:41 +0530

    Update blinkit.sql

--- blinkit.sql
+++ blinkit.sql
@@ -66,8 +66,81 @@ from blinkit_data
 group by outlet_location_type, item_fat_content 
 order by total_sales asc limit 5 ;
 
+SELECT
+    outlet_location_type,
+    CAST(SUM(CASE WHEN item_fat_content = 'Low Fat' THEN total_sales ELSE 0 END) AS DECIMAL(10,2)) AS low_fat,
+    CAST(SUM(CASE WHEN item_fat_content = 'Regular' THEN total_sales ELSE 0 END) AS DECIMAL(10,2)) AS regular
+FROM blinkit_data
+GROUP BY outlet_location_type
+ORDER BY outlet_location_type;
 
+select outlet_establishment_year,
+cast(sum(total_sales) as decimal(10,2)) as total_sales
+from blinkit_data
+group by outlet_establishment_year
+order by outlet_establishment_year asc;
 
+SELECT
+    outlet_size,
+    total_sales,
+    ROUND((total_sales * 100.0) / SUM(total_sales) OVER (), 2) AS sales_percentage
+FROM (
+    SELECT
+        outlet_size,
+        SUM(total_sales) AS total_sales
+    FROM blinkit_data
+    GROUP BY outlet_size
+) AS t
+ORDER BY total_sales desc;
+
+select outlet_location_type,
+cast(sum(total_sales) as decimal(10,2)) as total_sales,
+ROUND(sum(total_sales * 100.0) / (SELECT SUM(total_sales) FROM blinkit_data),
+        2
+    ) AS sales_percentage,
+cast(avg(total_sales) as decimal(10,2)) as Avg_sales,
+count(*) as no_of_items,
+cast(avg(rating) as decimal(10,2)) as avg_rating
+from blinkit_data
+group by outlet_location_type
+order by total_sales asc;
+
+SELECT
+    outlet_location_type,
+    SUM(total_sales) AS total_sales,
+    ROUND(
+        SUM(total_sales) * 100 /
+        (SELECT SUM(total_sales) FROM blinkit_data),
+        2
+    ) AS sales_percentage,
+    ROUND(AVG(total_sales), 2) AS avg_sales,
+    COUNT(*) AS no_of_items,
+    ROUND(AVG(rating), 2) AS avg_rating
+FROM blinkit_data
+GROUP BY outlet_location_type
+ORDER BY total_sales ASC;
+
+SELECT
+    outlet_type,
+    SUM(total_sales) AS total_sales,
+    ROUND(
+        SUM(total_sales) * 100 /
+        (SELECT SUM(total_sales) FROM blinkit_data),
+        2
+    ) AS sales_percentage,
+    ROUND(AVG(total_sales), 2) AS avg_sales,
+    COUNT(*) AS no_of_items,
+    ROUND(AVG(rating), 2) AS avg_rating
+FROM blinkit_data
+GROUP BY outlet_type
+ORDER BY total_sales desc;
+SELECT
+    outlet_establishment_year,
+    SUM(total_sales) AS total_sales
+FROM blinkit_data
+GROUP BY outlet_establishment_year
+ORDER BY total_sales desc
+LIMIT 1;
 
 
 
